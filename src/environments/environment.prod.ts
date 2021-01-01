4a428fb51fdcab5b436478bfa3e3c2250fe6d0f5
alejandro camba <alejandrocamba@MacBook-Pro-de-alejandro.local>
2021-01-01 17:53:33 -0400

    adding prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,17 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.api.gameitemprices.com/api'
+  apiUrl: 'https://www.api.gameitemprices.com/api',
+  endpoints: { // IMPORTANT TO DEFINE EVEN IF IT'S NOT USED
+  login: {
+    path: 'auth/login',
+    body: {
+      email: 'username',
+      password: 'password'
+    }
+  },
+  logout: {
+    path: 'auth/logout'
+  },
+  widthCredentials: true
+}
 };
